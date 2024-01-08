85946008d3222080dec51d169406d0dad1c0a0c5
nhan.nguyen <nhan.nguyen@innogram.co>
2024-01-08 09:41:16 +0700

    kanagawa theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,7 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  -- { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
