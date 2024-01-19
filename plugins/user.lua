82528acf758d7542ffa811de0187c107fd4e2a06
nhan.nguyen <nhan.nguyen@innogram.co>
2024-01-19 10:14:39 +0700

    add markdown preview

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,10 @@ return {
       }
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+}
 }
