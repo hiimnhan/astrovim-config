6e130c90c69c9b517a1577db9fd3cea5cfa38a39
nhanncv <vinhnhan1999bc@gmail.com>
2024-02-25 10:35:03 +0700

    add twoslash-query

--- plugins/user.lua
+++ plugins/user.lua
@@ -14,5 +14,15 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
-}
+  },
+  {
+    "marilari88/twoslash-queries.nvim",
+    config = function()
+      require("twoslash-queries").setup {
+        multi_line = true, -- to print types in multi line mode
+        is_enabled = true, -- to keep disabled at startup and enable it on request with the TwoslashQueriesEnable
+        highlight = "Type", -- to set up a highlight group for the virtual text
+      }
+    end,
+  },
 }
